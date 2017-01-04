bce399c85c865c4b9972a68a34a7ac34a4a218e5
GTconsult-A-Team <Craig@gtconsult.com>
2017-01-04 09:15:31 +0200

    Updated with Secure code

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // Add your requirements
+
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
+
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+
+//create bot and add dialogs
+/*
+var bot = new builder.BotConnectorBot
+({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_SECRET });
+bot.add('/', new builder.SimpleDialog(function(session){
+    session.send('Hello World');
+}));
+*/
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -11,11 +24,11 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourID', appPassword: 'YourPass' }); 
+({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_SECRET }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/API/Messages', connector.listen());
 
 // Create bot dialogs
-bot.dialog('/', function (session) {
+bot.dialog('/', new builder.SimpleDialog(function (session) {
     session.send("Hello World");
-});
+}));
